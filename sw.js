12a4d84728f67034bc4ea74c3ad1142bc239defc
kio0411 <1797099568@qq.com>
2026-08-16 11:55:52 +0800

    Fix card reveal and view switching

--- sw.js
+++ sw.js
@@ -1,11 +1,21 @@
-const CACHE = 'dianying-cards-v10';
+const CACHE = 'dianying-cards-v12';
 const ASSETS = ['./', './index.html', './additional-book.js', './reading-passages.js', './interview-questions.js', './manifest.webmanifest'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).catch(() => undefined).then(() => self.skipWaiting())));
+self.addEventListener('install', event => event.waitUntil(
+  caches.open(CACHE)
+    .then(cache => Promise.all(ASSETS.map(url => fetch(new Request(url, { cache:'reload' })).then(response => response.ok ? cache.put(url, response) : undefined).catch(() => undefined))))
+    .then(() => self.skipWaiting())
+));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).then(response => {
-    if (new URL(event.request.url).origin === self.location.origin) event.waitUntil(caches.open(CACHE).then(cache => cache.put(event.request, response.clone())).catch(() => undefined));
-    return response;
-  }).catch(() => caches.match(event.request).then(cached => cached || (event.request.mode === 'navigate' ? caches.match('./index.html') : undefined))));
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(caches.match(event.request).then(cached => {
+    const network = fetch(event.request).then(response => {
+      if (response.ok) event.waitUntil(caches.open(CACHE).then(cache => cache.put(event.request, response.clone())).catch(() => undefined));
+      return response;
+    }).catch(() => cached || (event.request.mode === 'navigate' ? caches.match('./index.html') : undefined));
+    const mustRefresh = event.request.mode === 'navigate' || /\.(?:html|js|webmanifest)$/.test(url.pathname);
+    return mustRefresh ? network : cached || network;
+  }));
 });
