def6a2057b0427794ed82bd1e4da663792f3aa62
kio0411 <1797099568@qq.com>
2026-08-16 10:02:21 +0800

    Add saved progress and wordbook

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dianying-cards-v8';
+const CACHE = 'dianying-cards-v9';
 const ASSETS = ['./', './index.html', './additional-book.js', './reading-passages.js', './interview-questions.js', './manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
